a011b0d698d18d7df64c88bb2f2d4c9a85775989
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-01-23 18:03:47 -0200

    Basic functions

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,17 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn favorites
+  [name & favorites]
+  (str name "'s favorites are: " (clojure.string/join ", " favorites)))
+
+(defn recursive-destructuring
+  [{:keys [start stop]}]
+  (println start)
+  (if (< start stop) (recursive-destructuring {:start (+ start 1) :stop stop})))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "main function."
   [& args]
-  (println "Hello, World!"))
+  (recursive-destructuring {:start 1 :stop 10})
+  (println (favorites "Bob" "ice cream" "toys" "coding")))
